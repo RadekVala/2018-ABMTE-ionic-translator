f6418aa2662f35b6806382317a1dc64497d4489d
Radek Vala <radek.www@gmail.com>
2018-11-02 11:11:25 +0100

    TranslationProvider generated, HttpClientModule added to app.modules  ionic generate provider TranslationProvider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TranslationProvider } from '../../providers/translation/translation';
 
 @Component({
   selector: 'page-home',
@@ -9,13 +10,18 @@ export class HomePage {
 
   private userInput:string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private translationProvider: TranslationProvider
+    ) {
 
   }
 
   public btnTranslateClicked(userInput:string): void{
     //console.log(userInput);
     this.userInput = userInput;
+
+
   }
 
 }
