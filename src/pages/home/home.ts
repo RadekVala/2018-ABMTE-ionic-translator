86e26337e8e84dc65dcc69110e322cccfd6d9de2
Radek Vala <radek.www@gmail.com>
2018-11-02 11:00:05 +0100

    btnTranslate Event Handler

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  private userInput:string;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  public btnTranslateClicked(userInput:string): void{
+    //console.log(userInput);
+    this.userInput = userInput;
   }
 
 }
